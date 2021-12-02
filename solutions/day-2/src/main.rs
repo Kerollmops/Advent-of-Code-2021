51bc57fb698731769976f6c8d7c801b47967c94d
Clément Renault <clement@meilisearch.com>
2021-12-02 11:47:01 +0100

    Answer for day 2 part 1

---
+++ solutions/day-2/src/main.rs
@@ -0,0 +1,64 @@
+use std::error::Error;
+use std::fmt;
+use std::str::FromStr;
+
+const INPUT: &str = include_str!("../../../inputs/day-2.txt");
+
+enum Movement {
+    Forward(i32),
+    Down(i32),
+    Up(i32),
+}
+
+#[derive(Debug, Clone, Copy)]
+struct InvalidInput;
+
+impl fmt::Display for InvalidInput {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("invalid input")
+    }
+}
+
+impl Error for InvalidInput {}
+
+impl FromStr for Movement {
+    type Err = InvalidInput;
+
+    fn from_str(s: &str) -> Result<Movement, InvalidInput> {
+        match s.split_once(' ') {
+            Some((m, c)) => {
+                let c = c.trim().parse().map_err(|_| InvalidInput)?;
+                match m {
+                    "forward" => Ok(Movement::Forward(c)),
+                    "down" => Ok(Movement::Down(c)),
+                    "up" => Ok(Movement::Up(c)),
+                    _ => Err(InvalidInput),
+                }
+            }
+            _ => Err(InvalidInput),
+        }
+    }
+}
+
+fn main() -> anyhow::Result<()> {
+    let movements = INPUT
+        .lines()
+        .map(Movement::from_str)
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let mut horizontal = 0;
+    let mut vertical = 0;
+
+    for movement in movements {
+        match movement {
+            Movement::Forward(n) => horizontal += n,
+            Movement::Down(n) => vertical += n,
+            Movement::Up(n) => vertical -= n,
+        }
+    }
+
+    let answer = horizontal * vertical;
+    println!("first answer is {}", answer);
+
+    Ok(())
+}
