6a5e441723246791cc609d887c0025542fb022dd
Clément Renault <clement@meilisearch.com>
2021-12-03 11:58:38 +0100

    Answer for day 3 part 2

--- solutions/day-3/src/main.rs
+++ solutions/day-3/src/main.rs
@@ -1,4 +1,5 @@
 use bitvec::prelude::*;
+use std::cmp::Ordering;
 
 const INPUT: &str = include_str!("../../../inputs/day-3.txt");
 
@@ -17,7 +18,7 @@ fn main() -> anyhow::Result<()> {
     let mut gamma_bits: BitVec = BitVec::new();
     let mut alpha_bits: BitVec = BitVec::new();
     for (i, count) in ones_counts.into_iter().enumerate() {
-        if dbg!(count > number_lines / 2) {
+        if count > number_lines / 2 {
             gamma_bits.push(true);
             alpha_bits.push(false);
         } else {
@@ -35,5 +36,56 @@ fn main() -> anyhow::Result<()> {
 
     println!("first answer is {}", answer);
 
+    let mut oxygen_generator_rating_lines = bits_lines.clone();
+    let oxygen_generator_rating = filter_lines(&mut oxygen_generator_rating_lines, 0, Common::Most);
+    let oxygen_generator_rating = isize::from_str_radix(oxygen_generator_rating, 2).unwrap();
+
+    let mut co2_scrubber_rating_lines = bits_lines.clone();
+    let co2_scrubber_rating = filter_lines(&mut co2_scrubber_rating_lines, 0, Common::Least);
+    let co2_scrubber_rating = isize::from_str_radix(co2_scrubber_rating, 2).unwrap();
+
+    let answer = oxygen_generator_rating * co2_scrubber_rating;
+    println!("second answer is {}", answer);
+
     Ok(())
+}
+
+enum Common {
+    Least,
+    Most,
+}
+
+fn filter_lines<'a>(lines: &mut Vec<&'a str>, position: usize, criteria: Common) -> &'a str {
+    if lines.len() == 1 {
+        lines[0]
+    } else {
+        let mut ones_count = 0;
+        let mut zeros_count = 0;
+        for line in lines.iter() {
+            if (line.chars().nth(position) == Some('1')) {
+                ones_count += 1;
+            } else {
+                zeros_count += 1;
+            }
+        }
+
+        let keep_bit = match ones_count.cmp(&zeros_count) {
+            Ordering::Less => match criteria {
+                Common::Most => '0',
+                Common::Least => '1',
+            },
+            Ordering::Equal => match criteria {
+                Common::Most => '1',
+                Common::Least => '0',
+            },
+            Ordering::Greater => match criteria {
+                Common::Most => '1',
+                Common::Least => '0',
+            },
+        };
+
+        lines.retain(|l| l.chars().nth(position) == Some(keep_bit));
+
+        filter_lines(lines, position + 1, criteria)
+    }
 }
